380a83858b89d04cafc751e3dc01879f87e1dcad
Star-codi <arjunrajan8@gmail.com>
2026-02-27 14:44:39 +0530

    Initial Spring Boot app

---
+++ src/main/java/com/arjun/cap/AppController.java
@@ -0,0 +1,20 @@
+package com.arjun.cap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class
+AppController
+{
+@GetMapping
+(
+"/hello"
+)
+public
+String hello() {
+return
+"Hello from DevOps Pipeline! 🚀"
+;
+}
+}
